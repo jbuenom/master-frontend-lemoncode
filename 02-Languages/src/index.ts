540df42719b93e49bdea600b4a13cdcb23a5f4cf
Javier Bueno <buenorz@gmail.com>
2023-06-08 19:17:37 +0200

    Add nuevo módulo Lenguaje

---
+++ 02-Languages/src/index.ts
@@ -0,0 +1,21 @@
+import "./01-practica/ejercicio-101.js";
+import "./01-practica/ejercicio-102.js";
+import "./01-practica/ejercicio-103.js";
+import "./01-practica/ejercicio-104.js";
+import "./01-practica/ejercicio-105.js";
+import "./01-practica/ejercicio-108.js";
+import "./01-practica/ejercicio-109.js";
+import "./01-practica/ejercicio-111.js";
+import "./01-practica/ejercicio-112.js";
+import "./01-practica/ejercicio-114.js";
+import "./01-practica/ejercicio-115.js";
+import "./01-practica/ejercicio-116.js";
+import "./01-practica/ejercicio-206.js";
+import "./02-entregables/ejercicio-01.js";
+import "./02-entregables/ejercicio-02.js";
+import "./02-entregables/ejercicio-03.js";
+import "./02-entregables/ejercicio-04.js";
+import "./02-entregables/ejercicio-05.js";
+import "./03-retos/ejercicio.js";
+
+console.log("Welcome to Exercises Playground.");
